c731128aea9e40f5c38dc82bad4b4b09fb0055a7
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-07-01 14:32:25 -0400

    initial commit

---
+++ PrntNghtmreFix.ps1
@@ -0,0 +1,11 @@
+﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+$Path = "C:\Windows\System32\spool\drivers"
+
+$Acl = Get-Acl $Path
+
+$Ar = New-Object  System.Security.AccessControl.FileSystemAccessRule("System", "Modify", "ContainerInherit, ObjectInherit", "None", "Deny")
+
+$Acl.AddAccessRule($Ar)
+
+Set-Acl $Path $Acl
